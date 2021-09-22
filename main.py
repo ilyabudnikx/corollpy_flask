b5d44b7bae48d72a2a65b5e8a427cf7785c400a0
Ilya Budnik <ilyabudnikxs@mail.ru>
2021-09-22 19:15:21 +0300

    Math 1.0

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+def mathf(x):
+    if x == 0 or x == -1:
+        print("Не знаю что тут должно быть")
+    else:
+        y = (1/math.sqrt(x)+math.cos(math.exp(x))+(math.cos(x) ** 2))/(((math.log(1+x) ** 2) + math.sqrt((math.exp(math.cos(x))) + (math.sin(3.14 *x ) ** 2)))**1/3)
+        print(y)
+
+
+mathf(x = float(input("Введите X")))
