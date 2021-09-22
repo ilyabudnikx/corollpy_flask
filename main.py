19d3fea787ca644ff1edcab8f683b0b41f6a6b21
Ilya Budnik <ilyabudnikxs@mail.ru>
2021-09-22 23:21:22 +0300

    vwr0.2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import math
 def mathf(x):
-    if x == 0 or x == -1:
-        print("Не знаю что тут должно быть")
-    else:
-        y = (1/math.sqrt(x)+math.cos(math.exp(x))+(math.cos(x) ** 2))/(((math.log(1+x) ** 2) + math.sqrt((math.exp(math.cos(x))) + (math.sin(3.14 *x ) ** 2)))**1/3)
-        print(y)
-
+    try:
+        if x == 0 or x == -1:
+            print("Не знаю что тут должно быть")
+        else:
+            y = (1/math.sqrt(x)+math.cos(math.exp(x))+(math.cos(x) ** 2))/(((math.log(1+x) ** 2) + math.sqrt((math.exp(math.cos(x))) + (math.sin(3.14 *x ) ** 2)))**1/3)
+            print(y)
+    except:
+        print("Error")
 
 mathf(x = float(input("Введите X")))
